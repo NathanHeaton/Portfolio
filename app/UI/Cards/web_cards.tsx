d6c2f0348ee608bcd418ebf30e10a708af0c525e
Nathan Heaton <nathanmheaton@gmail.com>
2025-03-11 10:57:59 +0000

    got images to show correctly on desktop mobile is bugged

--- app/UI/Cards/web_cards.tsx
+++ app/UI/Cards/web_cards.tsx
@@ -5,30 +5,32 @@ import clsx from "clsx"
 const Cards = [
     {
         id: "1",
+        size: "w-1/3",
+        enlarged : false,
+        // text and links
         title: "Blooming Flower Clock",
         subTitle: "SETU Carlow year 3",
         description: "Clock represented by blooming flowers telling the season and time of day growth and movement of the flower implies the time have some trivia about the flower blooming in this month able to scroll through other months past months have wilted flowers and ones in the future are just seeds. Responsive mobile and desktop design. first mock up done in photoshop then I made a sprite sheet in aseprite with each of flower and their stage of growths. see whats in bloom today.",
         webLink: "https://nathanheaton.github.io/bloomingFlowerClock/",
-        mainImage: "/img/Blooming flower image.png",
-        mainImageAlt: "pixel art flower",
-        secondaryImage: "/img/blooming flower sprites.png",
-        secondaryImageAlt: "pixel art flower sprite sheet",
-        size: "1/3",
+        // images
+        mainImage: {src:"/img/Blooming flower image.png", alt: "pixel art flower", width: 300, height : 500},
+        secondaryImage: {src:"/img/blooming flower sprites.png", alt: "pixel art flower sprite sheet", width: 4, height : 3},
+        // for short description
         shortDescription: "SETU Y3, Unconventional clock, based on flowers",
         tools: " HTML, CSS, JS, Aseprite and Adobe Photoshop"
 
     },
     {
         id: "2",
+        size: "w-2/3",
+        enlarged : false,
         title: "VLE (Virtual Learning Enviornment)",
         subTitle: "SETU Carlow - UX Measurement",
         description: "To see my entire report of my process and the steps involved in my process please view download the below PDF",
         webLink: "https://nathanheaton.github.io/bloomingFlowerClock/",
-        mainImage: "/img/Module page adjusted.png",
-        mainImageAlt: "VLE login page",
-        secondaryImage: "/img/blooming flower sprites.png",
-        secondaryImageAlt: "pixel art flower sprite sheet",
-        size: "2/3",
+        mainImage: {src:"/img/Module page adjusted.png", alt: "VLE login page", width: 800, height : 300},
+        secondaryImage: {src:"/img/blooming flower sprites.png", alt: "VLE login page", width: 4, height : 3},
+
         shortDescription: "SETU Y3, High fidelity Wireframes, work process for VLE UI design and UX Measurement",
         tools: "Figma, MS Forms and OBS(for user testing/recordings)"
 
@@ -44,16 +46,18 @@ export default function WebCards(){
         {Cards.map((card) =>// returns a link for each section in code projects
         {
             return (
-            <section key={card.id} className={clsx(`max-w-${card.size}`,"max-sm:w-full")} >
-                <div className="w-full">
-                    <div className="hover:flex hidden w-full h-full absolute justify-center"><h2 >{card.title}</h2></div>
-                    <Image 
-                        src={card.mainImage}
-                        alt={card.mainImageAlt}
-                        width={10000}
-                        height={500}
-                        className="w-full h-auto rounded-t-xl"
-                    />
+            <section key={card.id} className={clsx(card.size, "max-md:w-full relative flex flex-col p-2")} >
+                <div className="w-full h-full md:relative overflow-hidden aspect-[4/3]">
+                    <div className="hover:flex hidden h-auto relative justify-center"><h2 >{card.title}</h2></div>
+                        <Image
+                        src={card.mainImage.src}
+                        alt={card.mainImage.alt}
+                        // width={card.mainImage.width || 500}
+                        // height={card.mainImage.height || 500}
+                        fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 33vw"
+                        className="w-full h-auto rounded-t-xl object-cover overflow-clip"
+                        />                   
                 </div>
                 {/*</section><button class="back-button" onclick="shrinkCard(this, 33)" >back</button>*/}
                 <article className="hidden" >
@@ -65,12 +69,12 @@ export default function WebCards(){
                     <a href={card.webLink} id="button-link" target="_blank">Check it out</a>
                     <div className="Image-inner-card" >
                         <Image 
-                            src={card.secondaryImage} alt={card.secondaryImageAlt} width="100" height={100}
+                            src={card.secondaryImage.src} alt={card.secondaryImage.alt} width="100" height={100}
                             />
                     </div>
                 </article>  
-                <section className="short-description">
-                    <p className="bg-darkgrey rounded-b-xl p-4"><strong>Description</strong>: {card.shortDescription} <br />
+                <section className="bg-darkgrey rounded-b-xl">
+                    <p className="p-4 text-wrap"><strong>Description</strong>: {card.shortDescription} <br />
                         <strong>Tools</strong>: {card.tools}<br />
                         <i>Click/Tap image to find out more!</i></p>
                 </section>
--- app/code_projects/layout.tsx
+++ app/code_projects/layout.tsx
@@ -8,10 +8,10 @@ export default function CodeLayout({
   }>) {
     return (
         
-<div className="flex row flex-wrap items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
+<div className="flex flex-col flex-wrap items-center justify-items-center min-h-screen p-8 pb-20 gap-5 sm:p-20 ">
 
 
-    <section className="flex justify-center flex-wrap pt-25 w-full md:max-w-200 md:w-3/5">
+    <section className="flex justify-center flex-wrap py-25 w-full md:max-w-200 md:w-3/5">
         <h1 className="text-l py-5">Programming and UX/UI portfolio</h1>
         <p>Here is all of my programming and design work, showcasing my technical skills and focus on user
              experience. I developed games using C++ and SFML in my first year of IDAD and another game in
@@ -23,8 +23,8 @@ export default function CodeLayout({
         <CodeNav></CodeNav>
     </nav>
 
-    <main className="w-full flex justify-center max-w-400 md:w-5/6">
-    <section className="flex row gap-10 flex-wrap">
+    <main className="w-full flex max-w-400">
+    <section className="w-full flex flex-row flex-wrap">
         {children}
     </section>
     </main>
